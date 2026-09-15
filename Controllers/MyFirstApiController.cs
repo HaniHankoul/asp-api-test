d31c63b8df6a3a981750b6c80a09b713eb8f90e7
Hani <hankoolh@gmail.com>
2026-09-15 18:12:28 +0300

    first commit

---
+++ Controllers/MyFirstApiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyFirstApiController : ControllerBase
+    {
+        [HttpGet("MyName",Name ="MyName")]
+        public string GetMyName()
+        {
+            return "Hani :)";
+        }
+        [HttpGet("YourName",Name ="YourName")]
+        public string GetYourName()
+        {
+            return "Hello world :)";
+        }
+        [HttpGet("sum/{a}/{b}")]
+        public int SumTwo(int a,int b) {
+        return a+b;
+        }
+        [HttpGet("multiply/{a}/{b}")]
+        public int MulTwo(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
